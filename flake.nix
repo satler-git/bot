2d3bbfad98a3f84342bb2f3435f3bfeccf47693a
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-23 17:38:57 +0900

    chore: init flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.rust-overlay.overlays.default
+        ];
+      };
+
+      rust = pkgs.rust-bin.stable.latest.default;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust
+
+          wrangler
+          worker-build
+        ];
+      };
+    };
+}
