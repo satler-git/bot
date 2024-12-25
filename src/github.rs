1f94c8063c59aec073caf5247d808af46deab4e2
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-25 21:50:07 +0900

    feat: demock send comment

--- src/github.rs
+++ src/github.rs
@@ -13,6 +13,42 @@ pub enum EventType {
     _Unknown,
 }
 
+use github_webhook::payload_types as gh;
+use worker::Result;
+
+use reqwest::header;
+
+pub async fn comment_on_issue<'a>(
+    issue: &gh::Issue<'a>,
+    repo: &gh::Repository<'a>,
+    content: &str,
+    token: &str,
+) -> Result<()> {
+    let endpoint = format!(
+        "https://api.github.com/repos/{}/{}/issues/{}/comments",
+        repo.owner.login, repo.name, issue.number
+    );
+
+    let client = reqwest::Client::new();
+
+    client
+        .post(endpoint)
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header(header::AUTHORIZATION, format!("Bearer {token}"))
+        .header(header::ACCEPT, "application/vnd.github+json")
+        .header(header::USER_AGENT, crate::APP_NAME)
+        .body(
+            serde_json::json!({
+                "body": content,
+            })
+            .to_string(),
+        )
+        .send()
+        .await
+        .map_err(|e| worker::Error::RustError(format!("Error in sending a request: {e}")))
+        .map(|_| ())
+}
+
 impl From<&str> for EventType {
     fn from(v: &str) -> EventType {
         match v {
